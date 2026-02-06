41e7aeb4e90f79eed27bb8c3e64ffe52c10645ff
tobiobim-cpu <tobiobimuyiwa@gmail.com>
2026-02-06 10:19:36 -0500

    Add files via upload

---
+++ DataCamp/Microsoft_journey.py
@@ -0,0 +1,43 @@
+##The world is becoming more data-driven
+##Data and information is considered as the most strategic asset of a business, the underlying challenge that organiztions have today is 
+  #understanding and using their data to positively affect change within the business. This would help business decision making
+  #We should be able to use our data to tell a STORY! Business decision makers depend on an accurate story to drive better biz decision
+      #The faster a biz can make precise decisions, the more competitive they will be and the better advantage they will have
+  #You need to partner with data experts like data engineers, data scientists to help you get the data you need to tell a STORY!
+      #DATA CULTURE IS A MUST!
+     #You must be careful to tell the STORY to the right people at the right place.
+##Data Analysis help to overcome the challenge of finding insights and trends in data through story telling. You must know how to gather the right data and what to do with it.
+    #It is the process of identifying, cleaning, transforming, and modeling data to discover meaningful and useful info.
+    #The data is then crafted into a story through reports for analysis to suport the critical decision-making process.
+    #To analyze data, core components of analytics are divided into the following:
+       #1)Descriptive: it answers the questions of "what has happened based on historical data". It summarizes large datasets to describe outcomes to stakeholders
+          #Evaluates data through a process known as EXPLORATORY DATA ANALYSIS (EDA)
+          #developing KPIs and Metrics are key to measure track success or failure of key objective and performance in specific industries
+          #E.g. Generating reportsto provide a view of an organization's sales and financial data.
+        
+        #2)Diagnostic: it answers the questions "about why events happened". It uses findings from descriptive analytics to uncover cause of these events
+            #a)Identify anomalies in the data. These might be unexpected changes in a metric or a particular market
+            #b)Collect data thats related to these anomalies
+            #c)Use statistical tecniques to discover relationships and trends that explain these anomalies.
+        
+        #3)Predictive: it answers the question "about what will happen in the future". Uses historical data to identify trends and determine if they are likely to occur.
+            #techniques includes variety of statistical and ML techniques such as NN, decision trees, and regression.
+
+        #4)Presciptive: it answers the questions about "which actions should be taken to achieve a goal or target". Uses insights from predictive analytics.
+           #It allows biz to make informed decisions in the face of uncertainty. It relies on ML techniques to find patterns in large datasets
+           #By analyzing past decisions and events, orgnizations can estimate the likelihood of different outcomes.
+
+        #5)Cognitive: it draws inferences from existing data & patterns, derive conclusion based on existing knowledge bases and then add these findings back into the 
+             #knowledge base for future inferences, a self-learning feedback loop
+             #It helps you to learn what might happen if circumstances change and determine how you might handle these situations
+             #It depends on ML algos, and will use several NLP concepts to make sense of previously untapped data source such as call center conversation logs & product reviews.
+
+    #5 key ares needed for data analysis process:
+        #1)Prepare: it's the process of profiling, cleaning, and transforming your raw data to get it ready to model and visualize.
+        #2)Model: process of defining & creating relationships btw the tables. Your model has a direct effect on the performance of your report & overal data analysis.
+        #3)Visualize: this bring your data to life. The ultimate goal here is to solve biz problems. You need to think about the people who would be reading your data analysis report.
+        #4)Analyze: Is the important step of undertanding and interpreting the information that is displayed on the report.
+        #5)Manage: The management of your content helps to foster collaboration btw teams and individual.           
+
+
+### MICROSOFT POWER BI
---
+++ DataCamp/intro.py
@@ -0,0 +1,16 @@
+#Python is open source nd free. Its a general purpose scripting language.
+
+x = [["a", "b", "c"],
+     ["d", "e", "f"],
+     ["g", "h", "i"]]
+print(x[2][0])
+print(x[2][:2])
+
+p = ["a", "b", "c"]
+q = list(p)
+print (p)
+print(q, '\n')
+q[2] ='z'
+print(q)
+
+
